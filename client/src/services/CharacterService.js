e40dc7b0bfd433140fb29fc938ad03d9ca6393b3
emilykmcullen <emilycullen135@gmail.com>
2020-12-31 15:17:01 +0000

    Updated url for hosting

--- client/src/services/CharacterService.js
+++ client/src/services/CharacterService.js
@@ -1,31 +1,31 @@
-const baseURL = 'http://localhost:3000/api/characters/'
+const baseURL = 'https://irish-folklore-map.herokuapp.com/api/characters'
 
 export default {
     getCharacters(){
         return fetch(baseURL)
         .then(res => res.json())
-    },
-    postCharacter(payload){
-        return fetch(baseURL, {
-            method: 'POST',
-            body: JSON.stringify(payload),
-            headers: { 'Content-Type': 'application/json'}
-        })
-        .then(res => res.json())
-    },
-    updateCharacter(character) {
-        return fetch(baseURL + character._id, {
-          method: 'PUT',
-          body: JSON.stringify(character),
-          headers: { 
-            'Content-Type': 'application/json' 
-          }
-        })
-          .then(res => res.json());
-      },
-    deleteCharacter(id){
-        return fetch(baseURL + id, {
-            method: 'DELETE'
-        })
     }
+    // postCharacter(payload){
+    //     return fetch(baseURL, {
+    //         method: 'POST',
+    //         body: JSON.stringify(payload),
+    //         headers: { 'Content-Type': 'application/json'}
+    //     })
+    //     .then(res => res.json())
+    // },
+    // updateCharacter(character) {
+    //     return fetch(baseURL + character._id, {
+    //       method: 'PUT',
+    //       body: JSON.stringify(character),
+    //       headers: { 
+    //         'Content-Type': 'application/json' 
+    //       }
+    //     })
+    //       .then(res => res.json());
+    //   },
+    // deleteCharacter(id){
+    //     return fetch(baseURL + id, {
+    //         method: 'DELETE'
+    //     })
+    // }
 }
